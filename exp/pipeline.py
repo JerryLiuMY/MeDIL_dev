9ac383565f02ad2a7dd716a1d54fa91fdab833f6
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2023-01-19 02:00:28 -0600

    Update pipeline.py

--- exp/pipeline.py
+++ exp/pipeline.py
@@ -1,18 +1,20 @@
-from medil.functional_MCM import sample_from_minMCM
 from medil.functional_MCM import rand_biadj_mat
+from medil.functional_MCM import sample_from_minMCM
 from exp.estimation import estimation
+from learning.train import train_vae, valid_vae
+from learning.params import params_dict
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
+import torch
 import numpy as np
-import pandas as pd
-import time
 
 
-def pipeline(num_obs, edge_prob, num_samps=1000, seed=0):
+def pipeline(num_obs, edge_prob, seed=0):
     """ Pipeline function for estimating the shd and number of reconstructed latent
     Parameters
     ----------
     num_obs: number of observed variables
     edge_prob: edge probability
-    num_samps: number of samples
     seed: random seed
 
     Returns
@@ -25,49 +27,30 @@ def pipeline(num_obs, edge_prob, num_samps=1000, seed=0):
     num_latent_recon: number of reconstructed latent variables
     """
 
+    # load parameters
+    num_samps, batch_size = params_dict["num_samps"], params_dict["batch_size"]
+    num_train, num_valid = params_dict["num_train"], params_dict["num_valid"]
+
+    # create biadj_mat and samples
     np.random.seed(seed)
     biadj_mat = rand_biadj_mat(num_obs, edge_prob)
     samples, cov = sample_from_minMCM(biadj_mat, num_samps=num_samps)
 
+    # learn MeDIL model
     num_latent = biadj_mat.shape[0]
     biadj_mat_learned, shd, ushd, num_latent_recon = estimation(biadj_mat, num_obs, num_latent, samples)
+    m, n = biadj_mat_learned.shape
 
-    return num_latent, shd, ushd, num_latent_recon
+    # train generative models
+    train_samples, _ = sample_from_minMCM(biadj_mat, num_samps=num_train)
+    train_dataset = TensorDataset(torch.tensor(train_samples))
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
+    model, train_loss = train_vae(m, n, train_loader, biadj_mat_learned)
 
+    # perform validation on generative models
+    valid_samples, _ = sample_from_minMCM(biadj_mat, num_samps=num_valid)
+    valid_dataset = TensorDataset(torch.tensor(valid_samples))
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
+    valid_loss = valid_vae(model, valid_loader)
 
-def experiment(biadj_mat, num_runs, num_samps):
-    """ Experiment for verifying the SHD, USHD, diff and time
-    Parameters
-    ----------
-    biadj_mat: adjacency matrix of the generated graph
-    num_runs: number of runs
-    num_samps: number of samples in each run
-
-    Returns
-    -------
-    shd_df: dataframe of SHD
-    diff_df: dataframe of mis-specification in latent space
-    """
-
-    num_latent, num_obs = biadj_mat.shape
-    linspace = np.array(sorted(set([int(_) for _ in np.exp(np.linspace(2.5, np.log(num_samps), 10))])))
-    linspace = np.append(linspace, num_samps)
-    shd_df = pd.DataFrame(0., index=range(num_runs), columns=linspace)
-    ushd_df = pd.DataFrame(0., index=range(num_runs), columns=linspace)
-    diff_df = pd.DataFrame(0., index=range(num_runs), columns=linspace)
-    time_df = pd.DataFrame(0., index=range(num_runs), columns=linspace)
-
-    for i in range(num_runs):
-        samples, cov = sample_from_minMCM(biadj_mat, num_samps=num_samps)
-        for j in linspace:
-            print(f"Working on num_run={i} and num_sample={j}")
-            t0 = time.time()
-            biadj_mat_learned, shd, ushd, num_latent_recon = estimation(biadj_mat, num_obs, num_latent, samples[:j, :])
-            t1 = time.time()
-
-            shd_df.loc[i, j] = shd
-            ushd_df.loc[i, j] = ushd
-            diff_df.loc[i, j] = abs(num_latent - num_latent_recon)
-            time_df.loc[i, j] = t1 - t0
-
-    return shd_df, ushd_df, diff_df, time_df
+    return valid_loss
