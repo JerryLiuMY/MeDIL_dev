60eeeb01f61d02a2768026a27264d45ed9ceaa8c
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2023-01-20 00:46:41 -0600

    Update: Update experimental pipeline

--- exp/pipeline.py
+++ exp/pipeline.py
@@ -3,10 +3,10 @@ from medil.functional_MCM import sample_from_minMCM
 from exp.estimation import estimation
 from learning.train import train_vae, valid_vae
 from learning.params import params_dict
-from torch.utils.data import TensorDataset
-from torch.utils.data import DataLoader
-import torch
+from learning.data_loader import load_dataset
+from datetime import datetime
 import numpy as np
+import time
 
 
 def pipeline(num_obs, edge_prob, seed=0):
@@ -28,36 +28,36 @@ def pipeline(num_obs, edge_prob, seed=0):
     """
 
     # load parameters
+    np.random.seed(seed)
     num_samps, batch_size = params_dict["num_samps"], params_dict["batch_size"]
     num_train, num_valid = params_dict["num_train"], params_dict["num_valid"]
 
     # create biadj_mat and samples
-    np.random.seed(seed)
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Sampling from biadj_mat")
+    time.sleep(1)
     biadj_mat = rand_biadj_mat(num_obs, edge_prob)
-    samples, cov = sample_from_minMCM(biadj_mat, num_samps=num_samps)
+    samples, _ = sample_from_minMCM(biadj_mat, num_samps=num_samps)
 
     # learn MeDIL model
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Learning the MeDIL model")
     num_latent = biadj_mat.shape[0]
     biadj_mat_medil, _, _, _ = estimation(biadj_mat, num_obs, num_latent, samples)
-    m, n = biadj_mat_medil.shape
 
     # define training sample
-    train_samples, cov_train = sample_from_minMCM(biadj_mat, num_samps=num_train)
-    train_dataset = TensorDataset(torch.tensor(train_samples))
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
-
-    # define validation sample
-    valid_samples, cov_valid = sample_from_minMCM(biadj_mat, num_samps=num_valid)
-    valid_dataset = TensorDataset(torch.tensor(valid_samples))
-    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Preparing training and validation data for VAE")
+    train_loader, cov_train = load_dataset(biadj_mat, num_train, batch_size)
+    valid_loader, cov_valid = load_dataset(biadj_mat, num_valid, batch_size)
+    cov_train = cov_train[num_latent:, num_latent:]
+    cov_valid = cov_valid[num_latent:, num_latent:]
 
     # train & validate MeDIL VAE generative models
+    m, n = biadj_mat_medil.shape
     model_medil, _ = train_vae(m, n, train_loader, biadj_mat_medil, cov_train)
-    loss_medil = valid_vae(model_medil, valid_loader, cov_train)
+    loss_medil = valid_vae(model_medil, valid_loader, cov_valid)
 
     # train & validate Vanilla VAE generative models
-    biadj_mat_vanilla = np.ones((m, n))
-    model_vanilla, _ = train_vae(m, n, train_loader, biadj_mat_vanilla, cov_valid)
+    biadj_mat_vanilla = np.zeros((m, n))
+    model_vanilla, _ = train_vae(m, n, train_loader, biadj_mat_vanilla, cov_train)
     loss_vanilla = valid_vae(model_vanilla, valid_loader, cov_valid)
 
     return loss_medil, loss_vanilla
