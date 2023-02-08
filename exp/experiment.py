59408a22642dba8609c2822dcff4a0423c96c42c
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2023-02-07 19:40:53 -0600

    Update

--- exp/experiment.py
+++ exp/experiment.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from exp.examples import biadj_mat_list, conversion_dict
 from exp.pipeline import pipeline
+from exp.examples import biadj_mat_list, conversion_dict
 from medil.functional_MCM import rand_biadj_mat
 import os
 
@@ -55,3 +55,26 @@ def run_random(num_obs, edge_prob, linspace, alphas, exp_path):
                 if not os.path.isdir(folder_path):
                     os.mkdir(folder_path)
                     pipeline(biadj_mat, num_samps, alpha, folder_path, seed=0)
+
+
+def run_real(linspace, alphas, exp_path):
+    """ Run MeDIL on real dataset
+    Parameters
+    ----------
+    linspace: linspace for the number of samples
+    alphas: list of alphas
+    exp_path: path for the experiment
+    """
+
+    for i in range(10):
+        graph_path = os.path.join(exp_path, f"Real_{i}")
+        if not os.path.isdir(graph_path):
+            os.mkdir(graph_path)
+        for num_samps in linspace:
+            for alpha in alphas:
+                print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Working on real data {i} with "
+                      f"num_samps={num_samps} and alpha={alpha}")
+                folder_name = f"num_samps={num_samps}_alpha={alpha}"
+                folder_path = os.path.join(graph_path, folder_name)
+                if not os.path.isdir(folder_path):
+                    os.mkdir(folder_path)
