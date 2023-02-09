9fa674ed6cc7279f782c9cb5947e4d4d6fbfe456
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2023-02-08 18:46:51 -0600

    Update examples.py

--- exp/examples.py
+++ exp/examples.py
@@ -1,7 +1,8 @@
 import numpy as np
-conversion_dict = {0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h", 8: "i"}
+from medil.functional_MCM import rand_biadj_mat
 
-biadj_mat_list = [
+conversion_dict = {0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h", 8: "i"}
+fixed_biadj_mat_list = [
     np.array([[True, True]]),
     np.array([[True, True, True]]),
     np.array([[True, True, True, True]]),
@@ -27,3 +28,10 @@ biadj_mat_list = [
          [1, 1, 0, 0, 0, 1]], dtype=bool
     )
 ]
+
+
+num_obs = 10
+rand_biadj_mat_list = []
+for i in range(10):
+    np.random.seed(i)
+    rand_biadj_mat_list.append(rand_biadj_mat(num_obs=num_obs, edge_prob=np.random.uniform(size=1)[0]))
