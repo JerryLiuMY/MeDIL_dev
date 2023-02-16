4693651dc75cc2831ee9dfe3fd7029c1f66d1bc9
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2023-02-16 00:10:11 -0600

    Update

--- exp/pipeline.py
+++ exp/pipeline.py
@@ -1,16 +1,15 @@
+from exp.run_funcs import run_vae_oracle, run_vae_suite, run_vae_hrstc
 from medil.functional_MCM import sample_from_minMCM
 from learning.data_loader import load_dataset, load_dataset_real
 from graph_est.estimation import estimation, estimation_real
 from learning.params import params_dict
-from learning.train import train_vae
 from datetime import datetime
-import pickle
 import numpy as np
 import time
 import os
 
 
-def pipeline_graph(biadj_mat, num_samps, alpha, path, seed=0):
+def pipeline_graph(biadj_mat, num_samps, alpha, path, seed):
     """ Pipeline function for estimating the shd and number of reconstructed latent
     Parameters
     ----------
@@ -18,7 +17,7 @@ def pipeline_graph(biadj_mat, num_samps, alpha, path, seed=0):
     num_samps: number of samples used for adjacency matrix
     alpha: significance level
     path: path for saving the files
-    seed: random seed
+    seed: random seed for the experiments
     """
 
     # load parameters
@@ -50,11 +49,11 @@ def pipeline_graph(biadj_mat, num_samps, alpha, path, seed=0):
     cov_valid = cov_valid[num_latent:, num_latent:]
 
     # perform vae training
-    run_vae_oracle(biadj_mat, train_loader, valid_loader, cov_train, cov_valid, path)
-    run_vae_suite(biadj_mat_exact, biadj_mat_hrstc, train_loader, valid_loader, cov_train, cov_valid, path)
+    run_vae_oracle(biadj_mat, train_loader, valid_loader, cov_train, cov_valid, path, seed)
+    run_vae_suite(biadj_mat_exact, biadj_mat_hrstc, train_loader, valid_loader, cov_train, cov_valid, path, seed)
 
 
-def pipeline_real(dataset, alpha, path, seed=0):
+def pipeline_real(dataset, alpha, path, seed):
     """ Pipeline function for estimating the shd and number of reconstructed latent
     Parameters
     ----------
@@ -71,7 +70,6 @@ def pipeline_real(dataset, alpha, path, seed=0):
 
     # learn MeDIL model and save graph
     print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Learning the MeDIL model")
-
     biadj_mat_exact = estimation_real(samples, heuristic=False, alpha=alpha)
     biadj_mat_hrstc = estimation_real(samples, heuristic=True, alpha=alpha)
     np.save(os.path.join(path, "biadj_mat_exact.npy"), biadj_mat_exact)
@@ -83,63 +81,33 @@ def pipeline_real(dataset, alpha, path, seed=0):
     valid_loader = load_dataset_real(valid_samples, batch_size)
 
     cov_train, cov_valid = np.eye(samples.shape[1]), np.eye(samples.shape[1])
-    run_vae_suite(biadj_mat_exact, biadj_mat_hrstc, train_loader, valid_loader, cov_train, cov_valid, path)
+    run_vae_suite(biadj_mat_exact, biadj_mat_hrstc, train_loader, valid_loader, cov_train, cov_valid, path, seed)
 
 
-def run_vae_oracle(biadj_mat, train_loader, valid_loader, cov_train, cov_valid, path):
-    """ Run training loop for oracle VAE
+def pipeline_real_full(dataset, alpha, path, seed):
+    """ Pipeline function for estimating the shd and number of reconstructed latent
     Parameters
     ----------
-    biadj_mat: ground truth adjacency matrix
-    train_loader: loader for training data
-    valid_loader: loader for validation data
-    cov_train: covariance matrix for training data
-    cov_valid: covariance matrix for validation data
-    path: path to save the experiments
+    dataset: dataset for real experiments
+    alpha: significance level
+    path: path for saving the files
+    seed: random seed
     """
 
-    # train & validate oracle VAE
-    m, n = biadj_mat.shape
-    loss_true, error_true = train_vae(m, n, biadj_mat, train_loader, valid_loader, cov_train, cov_valid)
-    with open(os.path.join(path, "loss_true.pkl"), "wb") as handle:
-        pickle.dump(loss_true, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    with open(os.path.join(path, "error_true.pkl"), "wb") as handle:
-        pickle.dump(error_true, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    # load parameters
+    np.random.seed(seed)
+    batch_size = params_dict["batch_size"]
+    samples, valid_samples = dataset
 
+    # learn MeDIL model and save graph
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Learning the MeDIL model")
+    biadj_mat_hrstc = estimation_real(samples, heuristic=True, alpha=alpha)
+    np.save(os.path.join(path, "biadj_mat_hrstc.npy"), biadj_mat_hrstc)
 
-def run_vae_suite(biadj_mat_exact, biadj_mat_hrstc, train_loader, valid_loader, cov_train, cov_valid, path):
-    """ Run training loop for exact VAE
-    Parameters
-    ----------
-    biadj_mat_exact: adjacency matrix for exact graph
-    biadj_mat_hrstc: adjacency matrix for heuristic graph
-    train_loader: loader for training data
-    valid_loader: loader for validation data
-    cov_train: covariance matrix for training data
-    cov_valid: covariance matrix for validation data
-    path: path to save the experiments
-    """
+    # define VAE training and validation sample
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Preparing training and validation data for VAE")
+    train_loader = load_dataset_real(samples, batch_size)
+    valid_loader = load_dataset_real(valid_samples, batch_size)
 
-    # train & validate exact MeDIL VAE
-    me, ne = biadj_mat_exact.shape
-    loss_exact, error_exact = train_vae(me, ne, biadj_mat_exact, train_loader, valid_loader, cov_train, cov_valid)
-    with open(os.path.join(path, "loss_exact.pkl"), "wb") as handle:
-        pickle.dump(loss_exact, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    with open(os.path.join(path, "error_exact.pkl"), "wb") as handle:
-        pickle.dump(error_exact, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-    # train & validate heuristic MeDIL VAE
-    mh, nh = biadj_mat_hrstc.shape
-    loss_hrstc, error_hrstc = train_vae(mh, nh, biadj_mat_hrstc, train_loader, valid_loader, cov_train, cov_valid)
-    with open(os.path.join(path, "loss_hrstc.pkl"), "wb") as handle:
-        pickle.dump(loss_hrstc, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    with open(os.path.join(path, "error_hrstc.pkl"), "wb") as handle:
-        pickle.dump(error_hrstc, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-    # train & validate vanilla VAE
-    biadj_mat_vanilla = np.ones((me, ne))
-    loss_vanilla, error_vanilla = train_vae(me, ne, biadj_mat_vanilla, train_loader, valid_loader, cov_train, cov_valid)
-    with open(os.path.join(path, "loss_vanilla.pkl"), "wb") as handle:
-        pickle.dump(loss_vanilla, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    with open(os.path.join(path, "error_vanilla.pkl"), "wb") as handle:
-        pickle.dump(error_vanilla, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    cov_train, cov_valid = np.eye(samples.shape[1]), np.eye(samples.shape[1])
+    run_vae_hrstc(biadj_mat_hrstc, train_loader, valid_loader, cov_train, cov_valid, path, seed)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from exp.experiment import run_fixed
 from exp.experiment import run_random
 from exp.experiment import run_real
+from exp.experiment import run_real_full
 import numpy as np
 import os
 
@@ -13,22 +14,24 @@ def main(dataset_name, parent_path):
     parent_path: parent path for the experiments
     """
 
-    run = 0
     alphas = [0.01, 0.05, 0.1, 0.2, 0.5, 0.9]
-    exp_path = os.path.join(parent_path, f"experiment_{run}")
-    if not os.path.isdir(exp_path):
-        os.mkdir(exp_path)
+
+    for run in range(10):
+        # real dataset
+        exp_path = os.path.join(parent_path, f"experiment_{run}")
+        if not os.path.isdir(exp_path):
+            os.mkdir(exp_path)
+
         # fixed and random dataset
+        linspace = np.exp(np.linspace(np.log(100), np.log(2500), 10))
+        linspace = np.array(sorted(set(np.round(linspace)))).astype(int)
+        run_fixed(linspace, alphas, exp_path, seed=run)
+        run_random(linspace, alphas, exp_path, seed=run)
 
-    # linspace = np.exp(np.linspace(np.log(100), np.log(2500), 10))
-    # linspace = np.array(sorted(set(np.round(linspace)))).astype(int)
-    # run_fixed(linspace, alphas, exp_path)
-    # run_random(linspace, alphas, exp_path)
-
-    # real dataset
-    linspace = np.exp(np.linspace(np.log(100), np.log(632), 10))
-    linspace = np.array(sorted(set(np.round(linspace)))).astype(int)
-    run_real(dataset_name, linspace, alphas, exp_path)
+        linspace = np.exp(np.linspace(np.log(100), np.log(632), 10))
+        linspace = np.array(sorted(set(np.round(linspace)))).astype(int)
+        run_real(dataset_name, linspace, alphas, exp_path, seed=run)
+        # run_real_full(dataset_name, linspace, alphas, exp_path, seed=run)
 
 
 if __name__ == "__main__":
