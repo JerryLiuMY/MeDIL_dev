a0b9d8ec7f07064f5954af50177e737e2e221b60
Alex Markham <markham@kth.se>
2023-04-26 16:58:06 +0200

    updated random graph run

--- exp/examples.py
+++ exp/examples.py
@@ -3,41 +3,70 @@ import string
 from medil.functional_MCM import rand_biadj_mat
 
 # fixed graph
-conversion_dict = {0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h", 8: "i"}
+conversion_dict = {
+    0: "a",
+    1: "b",
+    2: "c",
+    3: "d",
+    4: "e",
+    5: "f",
+    6: "g",
+    7: "h",
+    8: "i",
+}
 fixed_biadj_mat_list = [
     np.array([[True, True]]),
     np.array([[True, True, True]]),
     np.array([[True, True, True, True]]),
     np.array([[True, True, False], [False, True, True]]),
-    np.array([[True, True, True, False, False],
-              [False, False, True, True, True]]),
-    np.array([[True, True, True, False, False, False, False],
-              [False, False, True, True, True, False, False],
-              [False, False, False, False, True, True, True]]),
-    np.array([[True, True, True, False, False, False, False, False, False],
-              [False, False, True, True, True, False, False, False, False],
-              [False, False, False, False, True, True, True, False, False],
-              [False, False, False, False, False, False, True, True, True]]),
-    np.array([[True, True, True, False, False, False, False, False, False, False, False],
-              [False, False, True, True, True, False, False, False, False, False, False],
-              [False, False, False, False, True, True, True, False, False, False, False],
-              [False, False, False, False, False, False, True, True, True, False, False],
-              [False, False, False, False, False, False, False, False, True, True, True]]),
+    np.array([[True, True, True, False, False], [False, False, True, True, True]]),
     np.array(
-        [[1, 1, 1, 0, 0, 0],
-         [1, 1, 0, 1, 0, 0],
-         [1, 1, 0, 0, 1, 0],
-         [1, 1, 0, 0, 0, 1]], dtype=bool
-    )
+        [
+            [True, True, True, False, False, False, False],
+            [False, False, True, True, True, False, False],
+            [False, False, False, False, True, True, True],
+        ]
+    ),
+    np.array(
+        [
+            [True, True, True, False, False, False, False, False, False],
+            [False, False, True, True, True, False, False, False, False],
+            [False, False, False, False, True, True, True, False, False],
+            [False, False, False, False, False, False, True, True, True],
+        ]
+    ),
+    np.array(
+        [
+            [True, True, True, False, False, False, False, False, False, False, False],
+            [False, False, True, True, True, False, False, False, False, False, False],
+            [False, False, False, False, True, True, True, False, False, False, False],
+            [False, False, False, False, False, False, True, True, True, False, False],
+            [False, False, False, False, False, False, False, False, True, True, True],
+        ]
+    ),
+    np.array(
+        [
+            [1, 1, 1, 0, 0, 0],
+            [1, 1, 0, 1, 0, 0],
+            [1, 1, 0, 0, 1, 0],
+            [1, 1, 0, 0, 0, 1],
+        ],
+        dtype=bool,
+    ),
 ]
 
 
 # random graph
-num_obs = [8, 15, ]
+num_obs_list = [10, 50, 100]
+edge_prob_list = np.arange(0.1, 0.91, 0.1)
+num_runs_per = 10
+
 rand_biadj_mat_list = []
-for i, num_obs in zip(range(10), num_obs):
-    np.random.seed(i)
-    rand_biadj_mat_list.append(rand_biadj_mat(num_obs=num_obs, edge_prob=np.random.uniform(size=1)[0]))
+
+for n in num_obs_list:
+    for p in edge_prob_list:
+        for run in range(num_runs_per):
+            rand_biadj_mat_list.append(rand_biadj_mat(num_obs=n, edge_prob=p))
 
 
 # tcga dataset
