98f099aaefd9c02af7e4ef4d0f1e186022b7e693
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2023-11-15 02:04:54 -0500

    Update train.py

--- learning/train.py
+++ learning/train.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import numpy as np
 import torch
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-torch.autograd.set_detect_anomaly(True)
 
 
 def train_vae(m, n, biadj_mat, train_loader, valid_loader, seed):
